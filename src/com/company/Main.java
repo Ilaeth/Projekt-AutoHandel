e14d7a011bf57ffe736f7a122a1d2d7a07857f43
Ilaeth <przemek1999.10.5@wp.pl>
2021-10-05 13:51:05 +0200

    Player

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Player player1 = new Player("Przemek", 1000000.0);
+
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,46 @@
 package com.company;
 
+import com.company.vehicles.Vehicle;
+import java.util.ArrayList;
+
 public class Player {
+    public String playerName;
+    private Double cash;
+    public ArrayList<Vehicle> playerGarage= new ArrayList<>();
+    public ArrayList<String> history=new ArrayList<>();
+
+    public Player(String playerName,Double cash){
+        this.playerName=playerName;
+        this.cash = cash;
+    }
+
+    public boolean PurchaseVehicle(Vehicle vehicle){
+        if(vehicle.price < cash){
+            System.out.println("Vehicle was purchased");
+            cash -= vehicle.price;
+            return true;
+        }
+        System.out.println("not enough Cash");
+        return false;
+    }
+
+    public void checkCash(){
+        System.out.println("Your account balance: "+Math.round(cash));
+    }
+
+    public void checkPlayerGarage(){
+        for(int i=0;i<playerGarage.size();i++){
+            System.out.println(i+1+". "+playerGarage.get(i));
+        }
+    }
+
+    public void showHistory(){
+        if (history.isEmpty()){
+            System.out.println("No history");
+        }else {
+            for(int i=0;i<history.size();i++){
+                System.out.println("Transaction number "+(i+1)+". "+history.get(i));
+            }
+        }
+    }
 }
